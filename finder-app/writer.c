7699b33942580415ed0e8059abb4a9b1d7a1cb8f
Moskwitto <kniteowl80.gmail.com>
2022-11-20 18:01:14 +0300

    writer.c 2.0

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <syslog.h>
-#include <string.h>
 int main(int argc,char *argv[]){
-    int counter=0,fd;
+    int counter=0;
+    int fd;
     openlog("writer-log", LOG_PID, LOG_USER);
     if(argc != 3) {printf("Not enough arguments specified");
-    exit(1);}
+    syslog(LOG_ERR, "Incorrect number of arguments. Received %d arguments instead of 2", argc-1);
+    return 1;}
      else{
          char writerfile[100];
          char writerstr[100];
@@ -15,11 +21,12 @@ int main(int argc,char *argv[]){
          printf("%s", writerfile);}
     //opening file to write
     FILE *file_pointer;
-    file_pointer=fopen(argv[1],"w");
-    if(file_pointer<0){
-    syslog(LOG_ERR, "Incorrect number of arguments. Received %d arguments instead of 2", argc-1);
-    exit(1);}
-     file_pointer=fopen(argv[1],"w");
+    //file_pointer=fopen(argv[1],"w");
+    fd=fd = open(argv[1], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    if(fd == -1){
+    printf("file not found!");}
+    file_pointer=fdopen(fd,"w");
+   
     char debug_message[100];
     strcpy(debug_message,"writing");
     strcpy(debug_message,argv[2]);
